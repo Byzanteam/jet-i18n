d49e89fa867beb54055865f9458588e81122cc52
Zheng Jie <zhengj89@icloud.com>
2020-05-18 11:23:44 +0800

    feat: add wechat push duo_on

--- lib/jet_share.ex
+++ lib/jet_share.ex
@@ -2,17 +2,4 @@ defmodule JetShare do
   @moduledoc """
   Documentation for JetShare.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> JetShare.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
