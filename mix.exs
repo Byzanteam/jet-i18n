e67af74937800c40d2283adbba5c21e63154c06e
Zheng Jie <zhengj89@icloud.com>
2020-05-18 13:45:03 +0800

    change i18next_to_gettext repo

--- mix.exs
+++ mix.exs
@@ -23,10 +23,8 @@ defmodule JetShare.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:i18next_to_gettext,
-       git: "git@github.com:ZhengQingchen/i18next-to-gettext.git",
+       git: "git@github.com:Byzanteam-Labs/i18next_to_gettext.git",
        branch: "zhengjie/feature/json-to-po"},
       {:gettext, ">= 0.0.0"}
     ]
