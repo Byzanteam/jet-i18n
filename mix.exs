a1c2ac1223dd110db21cde88094be282715a9d63
Zheng Jie <zhengj89@icloud.com>
2020-05-18 11:00:21 +0800

    feat: add convert json to po file

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule JetShare.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      compilers: [:gettext] ++ Mix.compilers(),
+      aliases: aliases()
     ]
   end
 
@@ -23,6 +25,18 @@ defmodule JetShare.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:i18next_to_gettext,
+       git: "git@github.com:ZhengQingchen/i18next-to-gettext.git",
+       branch: "zhengjie/feature/json-to-po"},
+      {:gettext, ">= 0.0.0"}
+    ]
+  end
+
+  defp aliases() do
+    [
+      "i18n.converte": [
+        "i18next_to_gettext.converte assets/locale/lang/zh_CN.json priv/gettext/zh_CN/LC_MESSAGES/default.po"
+      ]
     ]
   end
 end
