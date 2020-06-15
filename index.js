fe27cc8f3298d5e922d99aa8c9b9b80915c78a84
Phil Chen <06fahchen@gmail.com>
2020-06-15 23:09:57 +0800

    remove exports suffix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const defaultDomain = require("./assets/locale/lang/zh_CN.json");
 const gxzhSupervisionDomain = require("./assets/locale/lang/zh_CN.gxzh-supervision.json");
 
 exports.zh_CN = {
-  defaultDomain,
-  gxzhSupervisionDomain,
+  'default': defaultDomain,
+  'gxzh-supervision': gxzhSupervisionDomain,
 }
